500a16509f47886c9e00835a48f8da5b69a607b7
Matthew John Froggatt <mattfr@live.ca>
2023-04-16 14:06:27 +0200

    Remove a bunch of broken plugins

--- init.lua
+++ init.lua
@@ -1,11 +1,4 @@
--- TODO:
--- 	- extract configs to where they are needed (i.e. only
--- 	  configure something when I add it, not in this file)
--- 	- make testing work
---	- try this config out
-
 if vim.g.vscode == nil then
-
 
 	-- Helpers
 	local execute = vim.api.nvim_command
@@ -33,15 +26,13 @@ if vim.g.vscode == nil then
 
 		-- Install Packer plugins
 		require('plugins')
+		execute 'PackerClean'
 		execute 'PackerCompile'
 		execute 'PackerInstall'
 	end
 
 	-- Use configurations
 	require('plugins')
-
-	-- Completion setup
-	cmd 'autocmd BufEnter * lua require"completion".on_attach()'
 
 	-- Automatically compile and install Packer plugins
 	cmd 'autocmd BufWritePost plugins.lua lua require("plenary.reload").reload_module("plugins")'
@@ -58,36 +49,6 @@ if vim.g.vscode == nil then
 	vim.wo.signcolumn = 'yes'			-- Always show sign column
 	vim.wo.number = true				-- Print line number
 	vim.bo.smartindent = true			-- Insert indents automatically
-
-	-- Completion mappings
-	map('i', '<S-Tab>', 'pumvisible() ? "\\<C-p>" : "\\<S-Tab>"', {expr = true})
-	map('i', '<Tab>', 'pumvisible() ? "\\<C-n>" : "\\<Tab>"', {expr = true})
-	map('i', '<C-Space>', '<Plug>(completion_trigger)',{
-			silent = true,
-			noremap = false
-		}
-	)
-
-	-- LSP mappings
-	map('n', '<leader>,', '<cmd>lua vim.lsp.diagnostic.goto_prev()<CR>')
-	map('n', '<leader>;', '<cmd>lua vim.lsp.diagnostic.goto_next()<CR>')
-	map('n', '<leader>a', '<cmd>lua vim.lsp.buf.code_action()<CR>')
-	map('n', '<leader>d', '<cmd>lua vim.lsp.buf.definition()<CR>')
-	map('n', '<leader>f', '<cmd>lua vim.lsp.buf.formatting()<CR>')
-	map('n', '<leader>h', '<cmd>lua vim.lsp.buf.hover()<CR>')
-	map('n', '<leader>m', '<cmd>lua vim.lsp.buf.rename()<CR>')
-	map('n', '<leader>r', '<cmd>lua vim.lsp.buf.references()<CR>')
-	map('n', '<leader>s', '<cmd>lua vim.lsp.buf.document_symbol()<CR>')
-
-	-- LSP Utils config
-	lsp.handlers['textDocument/codeAction'] = require'lsputil.codeAction'.code_action_handler
-	lsp.handlers['textDocument/references'] = require'lsputil.locations'.references_handler
-	lsp.handlers['textDocument/definition'] = require'lsputil.locations'.definition_handler
-	lsp.handlers['textDocument/declaration'] = require'lsputil.locations'.declaration_handler
-	lsp.handlers['textDocument/typeDefinition'] = require'lsputil.locations'.typeDefinition_handler
-	lsp.handlers['textDocument/implementation'] = require'lsputil.locations'.implementation_handler
-	lsp.handlers['textDocument/documentSymbol'] = require'lsputil.symbols'.document_handler
-	lsp.handlers['workspace/symbol'] = require'lsputil.symbols'.workspace_handler
 
 	-- Remove background on sign column
 	execute 'highlight clear SignColumn'
